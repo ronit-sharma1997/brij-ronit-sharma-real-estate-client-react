9215e2ecdf56c276ea3cad97e3c763871f47a11f
Ron Sharma <sharma.ro@husky.neu.edu>
2024-03-15 23:39:37 -0400

    feat: add underline animations to nav links

--- src/common/components/NavBar.tsx
+++ src/common/components/NavBar.tsx
@@ -16,19 +16,19 @@ const NavBar: React.FC = () => {
       </div>
       {/* Right Side - Navigation Menus/Links */}
       <div className="hidden min-[900px]:flex mx-auto mt-5 xl:mt-0 xl:mx-0">
-        <div className="font-roboto-serif text-black uppercase font-medium text-base my-auto mx-7 hover:underline hover:cursor-pointer">
+        <div className="font-roboto-serif text-black uppercase font-medium text-base my-auto mx-7 relative after:bg-black after:absolute after:h-[1px] after:w-0 after:bottom-0 after:left-0 hover:after:w-full after:transition-all after:duration-300 cursor-pointer">
           Property Search
         </div>
-        <div className="font-roboto-serif text-black uppercase font-medium text-base my-auto mx-7 hover:underline hover:cursor-pointer">
+        <div className="font-roboto-serif text-black uppercase font-medium text-base my-auto mx-7 relative after:bg-black after:absolute after:h-[1px] after:w-0 after:bottom-0 after:left-0 hover:after:w-full after:transition-all after:duration-300 cursor-pointer">
           About Us
         </div>
-        <div className="font-roboto-serif text-black uppercase font-medium text-base my-auto mx-7 hover:underline hover:cursor-pointer">
+        <div className="font-roboto-serif text-black uppercase font-medium text-base my-auto mx-7 relative after:bg-black after:absolute after:h-[1px] after:w-0 after:bottom-0 after:left-0 hover:after:w-full after:transition-all after:duration-300 cursor-pointer">
           Stories
         </div>
-        <div className="font-roboto-serif text-black uppercase font-medium text-base my-auto mx-7 hover:underline hover:cursor-pointer">
+        <div className="font-roboto-serif text-black uppercase font-medium text-base my-auto mx-7 relative after:bg-black after:absolute after:h-[1px] after:w-0 after:bottom-0 after:left-0 hover:after:w-full after:transition-all after:duration-300 cursor-pointer">
           Contact
         </div>
-        <div className="font-roboto-serif text-black uppercase font-medium text-base my-auto ml-7 hover:underline hover:cursor-pointer">
+        <div className="font-roboto-serif text-black uppercase font-medium text-base my-auto ml-7 relative after:bg-black after:absolute after:h-[1px] after:w-0 after:bottom-0 after:left-0 hover:after:w-full after:transition-all after:duration-300 cursor-pointer">
           Home Valuation
         </div>
       </div>
@@ -40,7 +40,7 @@ const NavBar: React.FC = () => {
           viewBox="0 0 24 24"
           strokeWidth={1.5}
           stroke="currentColor"
-          className="w-10 h-10 text-black my-auto hover:bg-gray-100 hover:cursor-pointer"
+          className="w-10 h-10 text-black my-auto transition ease-linear hover:bg-gray-100 hover:cursor-pointer duration-300"
         >
           <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
         </svg>
