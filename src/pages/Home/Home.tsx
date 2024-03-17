a56a541f8aa132260a6b320e127e81b9759fc71a
Ron Sharma <sharma.ro@husky.neu.edu>
2024-03-16 23:30:15 -0400

    feat-common: add property search dropdown in navbar

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,6 +1,12 @@
 import NavBar from '../../common/components/NavBar.tsx';
 
 const Home: React.FC = () => {
-  return <NavBar />;
+  return (
+    <NavBar
+      parentClassName={
+        'flex flex-wrap justify-between px-5 py-5 md:px-10 md:py-5 bg-gradient-to-b from-black text-white'
+      }
+    />
+  );
 };
 export default Home;
