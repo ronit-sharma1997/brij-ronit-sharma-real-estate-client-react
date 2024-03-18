5232bf57109bb8de73ed45f7068a6d29719c3ac8
Ron Sharma <sharma.ro@husky.neu.edu>
2024-03-18 16:46:04 -0400

    feat-home: Add BuyRentSoldSearchBar

---
+++ src/pages/Home/BuyRentSoldSearchBar.tsx
@@ -0,0 +1,25 @@
+import SearchBar from '../../common/components/SearchBar.tsx';
+
+const BuyRentSoldSearchBar: React.FC = () => {
+  return (
+    <div className="absolute bottom-[7%] left-1/2 -translate-x-1/2 -translate-y-1/4">
+      <ul className="flex text-sm font-roboto-serif font-medium text-center">
+        <li className="me-1">
+          <div className="inline-block px-8 py-3 text-white bg-black rounded-t-lg active cursor-pointer">Buy</div>
+        </li>
+        <li className="me-1">
+          <div className="inline-block px-8 py-3 rounded-t-lg text-white bg-black/60 hover:text-gray-600 hover:bg-gray-50 cursor-pointer">
+            Rent
+          </div>
+        </li>
+        <li className="me-1">
+          <div className="inline-block px-8 py-3 rounded-t-lg text-white bg-black/60 hover:text-gray-600 hover:bg-gray-50 cursor-pointer">
+            Sold
+          </div>
+        </li>
+      </ul>
+      <SearchBar />
+    </div>
+  );
+};
+export default BuyRentSoldSearchBar;
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,5 @@
 import NavBar from '../../common/components/NavBar.tsx';
-import SearchBar from '../../common/components/SearchBar.tsx';
+import BuyRentSoldSearchBar from './BuyRentSoldSearchBar.tsx';
 
 const Home: React.FC = () => {
   return (
@@ -10,24 +10,7 @@ const Home: React.FC = () => {
           'flex flex-wrap justify-between px-5 py-5 md:px-10 md:py-5 bg-gradient-to-b from-black text-white absolute top-0 w-full'
         }
       />
-      <div className="absolute bottom-[7%] left-1/2 -translate-x-1/2 -translate-y-1/4">
-        <ul className="flex text-sm font-medium text-center">
-          <li className="me-1">
-            <a className="inline-block px-8 py-3 text-white bg-black rounded-t-lg active">Buy</a>
-          </li>
-          <li className="me-1">
-            <a className="inline-block px-8 py-3 rounded-t-lg text-white bg-black/60 hover:text-gray-600 hover:bg-gray-50">
-              Rent
-            </a>
-          </li>
-          <li className="me-1">
-            <a className="inline-block px-8 py-3 rounded-t-lg text-white bg-black/60 hover:text-gray-600 hover:bg-gray-50">
-              Sold
-            </a>
-          </li>
-        </ul>
-        <SearchBar></SearchBar>
-      </div>
+      <BuyRentSoldSearchBar />
     </div>
   );
 };
