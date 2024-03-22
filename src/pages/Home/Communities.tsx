0299e525fced49a3ecd7eeb44f1af25251a60304
Ron Sharma <sharma.ro@husky.neu.edu>
2024-03-21 21:00:35 -0400

    feat-home: add 'Show All Areas' button to communities section

--- src/pages/Home/Communities.tsx
+++ src/pages/Home/Communities.tsx
@@ -12,6 +12,11 @@ const Communities: React.FC = () => {
       <Community cityImgUrl={'Home/piscataway.jpg'} cityName={'Piscataway'} />
       <Community cityImgUrl={'Home/new-brunswick.jpg'} cityName={'New Brunswick'} />
       <Community cityImgUrl={'Home/perth-amboy.JPG'} cityName={'Perth Amboy'} />
+      <div className="w-full min-h-[287px] md:w-1/2 xl:w-1/3 2xl:w-1/5 relative">
+        <div className="rounded-none border-2 px-2 py-4 border-white font-roboto-serif text-sm uppercase text-white absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 hover:bg-white hover:cursor-pointer hover:text-black transition-[all] duration-700">
+          Show all Areas
+        </div>
+      </div>
     </div>
   );
 };
