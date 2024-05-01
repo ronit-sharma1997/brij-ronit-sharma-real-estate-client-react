765d2f3a84e43264d00dfd098d7b00e077e01796
Ron Sharma <sharma.ro@husky.neu.edu>
2024-04-30 21:20:27 -0400

    feat-common: add react routing

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,24 @@ import './App.css';
 import Home from './pages/Home';
 import AboutUs from './pages/AboutUs';
 import PropertySearch from './pages/PropertySearch';
+import { Route, Routes } from 'react-router-dom';
+import Stories from './common/components/Stories.tsx';
+import StoryPage from './pages/Stories/StoryPage.tsx';
+import ContactUs from './pages/ContactUs/ContactUs.tsx';
 
 function App() {
-  return <PropertySearch />;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/property-search" element={<PropertySearch />}></Route>
+        <Route path="/about-us" element={<AboutUs />}></Route>
+        <Route path="/stories" element={<StoryPage />}></Route>
+        <Route path="/contact-us" element={<ContactUs />}></Route>
+        <Route path="/home-valuation" element={<AboutUs />}></Route>
+      </Routes>
+    </>
+  );
 }
 
 export default App;
